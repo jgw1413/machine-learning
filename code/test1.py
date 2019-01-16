f25c03934637d149fe16b3cb68a8eb5ab3f3d5ea
jgw1413 <rhksdn1413@gmail.com>
2019-01-16 11:29:43 +0900

    소스 주석 설명 추가 / sigmoi -> tf.nn.relu

--- code/test1.py
+++ code/test1.py
@@ -13,14 +13,17 @@ train = pd.read_csv("../input/train.csv", index_col=0)
 test = pd.read_csv("../input/test.csv", index_col=0)
 
 # Really simple data preparation
+# 정말 간단한 데이터 준비
 y_train = pd.get_dummies(train[["type"]], prefix="")
 train.drop("type", inplace=True, axis=1)
 
 train_test = pd.concat([train, test], axis=0)
 
 # It looks like the color actually is just noise, and does not give any signal to the monster-class.
+# 이 색은 실제로는 단지 소음일 뿐이고, 괴물 등급에 어떠한 신호도 주지 않는 것처럼 보인다.
 # Comment one of these lines.
-#train_test = pd.get_dummies( train_test, columns=["color"], drop_first=False)
+# 이 선들 중 하나를 댓글로 달아.
+# train_test = pd.get_dummies( train_test, columns=["color"], drop_first=False)
 train_test.drop("color", inplace=True, axis=1)
 
 X_train = train_test.iloc[:len(y_train)]
@@ -32,7 +35,8 @@ del train
 del test
 
 ## A dead simple neural network class in Python+Numpy. Plain SGD, and no regularization.
-def sigmoid(X):
+# Python+Numpy의 죽은 간단한 신경 네트워크 클래스. 일반 SGD, 정규화 없음.
+def sigmoid(X):			#
     return 1.0 / ( 1.0 + np.exp(-X) )
 
 def softmax(X):
@@ -48,7 +52,7 @@ class neuralnet(object):
 
     def forward(self,X):
         net1 = np.matmul( X, self._W1 ) + self._b1
-        y = sigmoid(net1)
+        y = tf.nn.relu(net1)
         net2 = np.matmul( y, self._W2 ) + self._b2
         z = softmax(net2)
         return z,y
@@ -66,14 +70,17 @@ class neuralnet(object):
         self._b1 -= eta * d1
         
 # Some hyper-parameters to tune.
+# 튜닝할 하이퍼 파라미터.
 num_hidden = 12
 n_epochs   = 2000
 eta        = 0.01
 # Create the net.
+# 그물을 만들어라.
 nn = neuralnet( X_train.shape[1], num_hidden, y_train.shape[1])
 
 
 # (EDIT: It's much faster to convert the dataframes to numpy arrays and then iterate)
+# (EDIT: 데이터 프레임을 numpy 어레이로 변환한 다음 반복하는 것이 훨씬 더 빠름)
 X = np.array(X_train, dtype=np.float32)
 Y = np.array(y_train, dtype=np.float32)
 for epoch in range(n_epochs):
